35748854560d6883068fb724398bed7be50efe7e
DelioAraujo <delioaraujo@gmail.com>
2023-06-01 13:27:35 -0300

    req 2 passando no cy

--- src/context/TableProvider.js
+++ src/context/TableProvider.js
@@ -5,7 +5,7 @@ import tableContext from './TableContext';
 import useTableFetch from '../hooks/useTableFetch';
 
 function TableProvider({ children }) {
-  // tableData é o retorno da API com TODOS os planetas
+  // tableData é o retorno da API com TODOS os planetas.
   const { tableData } = useTableFetch();
   // planetType é a string capturada ao se digitar no imput Planet da barra de filtros.
   const { planetTyped } = useContext(filterContext);
