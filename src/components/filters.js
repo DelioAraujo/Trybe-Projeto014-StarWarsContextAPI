3fa79f0b7f0e40a801552139c5ea176a1d42ea69
DelioAraujo <delioaraujo@gmail.com>
2023-06-02 10:26:26 -0300

    req 3 passando

--- src/components/filters.js
+++ src/components/filters.js
@@ -63,11 +63,11 @@ function Filters() {
           value={ filterState.column }
           onChange={ handleColumnChange }
         >
-          <option value="population">Population</option>
-          <option value="orbital_period">Orbital Period</option>
-          <option value="diameter">Diameter</option>
-          <option value="rotation_period">Rotation Period</option>
-          <option value="surface_water">Surface Water</option>
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
         </select>
       </label>
 
